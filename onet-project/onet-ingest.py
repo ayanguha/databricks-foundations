5ba4ccfe3416609337901887c3ce06e5809c51bd
ayanguha <ayanguha>
2024-12-27 12:02:17 +0000

    initial commit

---
+++ onet-project/onet-ingest.py
@@ -0,0 +1,5 @@
+# Databricks notebook source
+urls = ['https://www.onetcenter.org/dl_files/database/db_29_1_text/Knowledge.txt', 
+        'https://www.onetcenter.org/dl_files/database/db_29_1_text/Skills.txt',
+        'https://www.onetcenter.org/dl_files/database/db_29_1_text/Abilities.txt',
+        'https://www.onetcenter.org/dl_files/database/db_29_1_text/Education%2C%20Training%2C%20and%20Experience.txt']
